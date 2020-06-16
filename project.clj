0e384c139e7e30f78270f981b6d3c1573cb354bd
Aaron Eberhart <aaron.eberhart@gmail.com>
2020-06-16 17:47:28 -0500

    initial big commit

--- project.clj
+++ project.clj
@@ -2,10 +2,6 @@
 	:description "FIXME: write description"
 	:dependencies [[org.clojure/clojure "1.10.1"]
 								 [slingshot "0.12.2"]
-								 ;[org.clojure/tools.logging "1.0.0"]
-								 ;[org.clojure/tools.trace "0.7.9"]
-								 ;[net.sourceforge.owlapi/owlapi-distribution "5.1.0"]
-								 ;[org.apache.jena/jena-core "3.2.0"]
 								 ;[com.hermit-reasoner/org.semanticweb.hermit "1.3.8.4"]
 								 ;[net.sourceforge.owlapi/pellet-owlapi-ignazio1977 "2.4.0-ignazio1977" :exclusions [net.sourceforge.owlapi/owlapi-distribution]]
 								 ]
