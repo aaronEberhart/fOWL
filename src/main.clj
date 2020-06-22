b16519bd6a546c2f9c7fe06440742964a11ac14b
Aaron Eberhart <aaron.eberhart@gmail.com>
2020-06-22 18:29:38 -0500

    documented all functions exported to main

--- src/main.clj
+++ src/main.clj
@@ -1,4 +1,5 @@
 (ns main
+ (:use [clojure.repl])
  (:require [ontology.IO :refer :all]
            [util.msc :as msc]
            [clojure.string :as str][clojure.java.io :as io]))
@@ -6,23 +7,19 @@
 
 (defn -main [& args]
  "Example Usage"
- (doseq [x 
-        [(classImplication (existential "R" "a") "b")
-         (classImplication (-or "b" "c") 
-                           (-not (-or "d" "e")))
-         (roleImplication (roleChain "r" (inverseRole "s")) "t")
-         (roleFact (inverseRole "s") "i" "j")
-         (classFact "A" "i")
-         (dataRoleFact "D" "i" (stringLiteral "l"))
-         (classImplication (<=role 4 "r" "c")
-                           (-not (-or (-and "d" "e") (-not (-and "f" "g")))))
-         (getNNF (classImplication (<=role 4 "r" "c") 
-                                   (-not (-or (-and "d" "e") (-not (-and "f" "g"))))))
-         (let [ont emptyOntology
-               ont (setOntologyIRI ont "<http://www.test.stuff>")
-               ont (addPrefix ont (prefix ":" "<http://www.test.stuff>"))
-               ont (addAxioms ont #{(classImplication ":a" ":b")(classImplication ":b" ":c")(classImplication ":d" ":a")})
-               _ (makeOWLFile "test.owl" ont)]
-         "done")         
-         ]]
-  (println x)))
+ (doc makeOWLFile)
+ (doseq [x [(classImplication (existential "r" "a") "b")
+            (classImplication (-or "b" "c") (-not (-or "d" "e")))
+            (roleImplication (roleChain "r" (inverseRole "s")) "t")
+            (roleFact (inverseRole "s") "i" "j")
+            (classFact "A" "i")
+            (dataRoleFact "D" "i" (stringLiteral "l"))
+            (classImplication (<=role 4 "r" "c") (-not (-or (-and "d" "e") (-not (-and "f" "g")))))
+            (getNNF (classImplication (<=role 4 "r" "c") (-not (-or (-and "d" "e") (-not (-and "f" "g"))))))
+            (let [ont emptyOntology
+                  ont (setOntologyIRI ont "<http://www.test.stuff>")
+                  ont (addPrefix ont (prefix ":" "<http://www.test.stuff>"))
+                  ont (addAxioms ont #{(classImplication ":a" ":b")(classImplication ":b" ":c")(classImplication ":d" ":a")})
+                  _ (makeOWLFile "test.owl" ont)]
+            "done")]]
+ (println x)))
