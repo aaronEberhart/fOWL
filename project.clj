bc1d56c033c5700fe0e877091b0d4a5c9b7c8e85
Aaron Eberhart <aaron.eberhart@gmail.com>
2020-06-25 16:29:47 -0500

    improving prefix adding functions

--- project.clj
+++ project.clj
@@ -1,10 +1,7 @@
 (defproject FOWL "0.1.0"
  :description "FIXME: write description"
  :dependencies [[org.clojure/clojure "1.10.1"]
-         [slingshot "0.12.2"]
-         ;[com.hermit-reasoner/org.semanticweb.hermit "1.3.8.4"]
-         ;[net.sourceforge.owlapi/pellet-owlapi-ignazio1977 "2.4.0-ignazio1977" :exclusions [net.sourceforge.owlapi/owlapi-distribution]]
-         ]
+                [slingshot "0.12.2"]]
  :main main
  :target-path "target/%s"
  :profiles {:uberjar {:aot :all}})
