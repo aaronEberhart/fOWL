e44e52b5397ae23749c8097989f8de61f4037aa0
Aaron Eberhart <aaron.eberhart@gmail.com>
2020-07-17 20:00:14 -0500

    .

--- project.clj
+++ project.clj
@@ -3,9 +3,5 @@
  :url "https://github.com/aaronEberhart/fOWL.git"
  :license {:name "Don't steal my stuff"}
  :dependencies [[org.clojure/clojure "1.10.1"]]
- :aot [main]
  :main main 
- :deploy-releases [["releases"  {:sign-releases false :url "https://clojars.org/projects"}]
-                   ["snapshots" {:sign-releases false :url "https://clojars.org/projects"}]]
  :profiles {:uberjar {:aot :all}})
- ;CLOJARS_19743277de649a870db38ce46b309e1ae0db53681b8e39a75baa455ea5a7
--- src/main.clj
+++ src/main.clj
@@ -2,10 +2,10 @@
  (:gen-class)
  (:use [clojure.repl])
  (:require [ontology.functions :refer :all]
-           [clojure.string :as str][clojure.java.io :as io]))
+           [clojure.string :as str][clojure.java.io :as io][clojure.main :as mn]))
 
 (defn -main [& args]
-	(prn "AAAA")
+ 
  )
 
 
