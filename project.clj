d8a3ab8c3769e39a3b8074a9340582ea28dcf529
Aaron Eberhart <aaron.eberhart@gmail.com>
2020-07-21 09:18:19 -0500

    testing something

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject onto.aaroneberhart/fowl "0.0.1-SNAPSHOT"
  :description "(f OWL) is a minimalistic functional programming style ontology editor"
- :url "https://github.com/aaronEberhart/fOWL.git"
+ :url "https://github.com/aaronEberhart/fOWL"
  :scm {:name "git" :url "https://github.com/aaronEberhart/fOWL"}
  :license {:name "Don't steal my stuff"}
  :dependencies [[org.clojure/clojure "1.10.1"]]
